74f76ba80066e8a22b7b516ae3dc7615b82018b2
Ananthakrishnan VK <krishnaananthvk@gmail.com>
2025-09-23 01:12:27 +0530

    updated basepath for react-router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter(
     },
     { path: "/login", element: <Login /> },
   ],
-  { basename: "/potholes/" },
+  { basename: "/potholes" },
 );
 
 export default router;
